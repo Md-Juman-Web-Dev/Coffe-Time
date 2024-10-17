c676fa61c1175aa727cd3796e3d4b4d76be015c0
SadmanSalim <sadmanmd225@gmail.com>
2024-10-17 13:08:26 +0600

    fixed swipper and added form action

--- JavaScript/Script.js
+++ JavaScript/Script.js
@@ -1,9 +1,9 @@
 var swiper = new Swiper('.mySwiper', {
-  slidesPerView: 4,
-  spaceBetween: 30,
-  centeredSlides: true,
-  pagination: {
-    el: '.swiper-pagination',
-    clickable: true,
+  slidesPerView: 5,
+  spaceBetween: 15,
+  navigation: {
+    nextEl: ".swiper-button-next",
+    prevEl: ".swiper-button-prev",
   },
+  centeredSlides: false,
 });
